a67530c0b4f121bbd6100c70c56ec6ec93c4bee4
dapangmaox <oliver.xinwei.liang@gmail.com>
2025-02-26 14:49:15 +0800

    feat: add theme class

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { Button } from './components/ui/button';
 
 function App() {
   return (
-    <>
+    <div className="bg-background text-foreground">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Button>TEST</Button>
-    </>
+    </div>
   );
 }
 
